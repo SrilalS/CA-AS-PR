ee67d715e654a680d15ccf3d6e51701d8ee1ced3
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2019-10-01 11:17:03 +0530

    Create V2.C

---
+++ V2.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    clock_t begin = clock();
+
+    FILE *ftxtfile;
+
+    ftxtfile = fopen("C://CA//Output.txt","w");
+
+    for (int i = 0; i <=100 ; ++i) {
+        fprintf(ftxtfile,"%d\n",i);
+        printf("%d\n",i);
+
+    }
+    system("getmac");
+
+    clock_t end = clock();
+
+    float time = (float)(end-begin)/CLOCKS_PER_SEC;
+
+    fprintf(ftxtfile,"Total Time Spent : %f",time);
+
+    fclose(ftxtfile);
+}
